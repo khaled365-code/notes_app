7baebf6d5dc18b4ad75bd845fd721109828c314b
HW <khaleddortmound123@gmail.com>
2023-10-12 20:43:14 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Notesapp());
+}
+
+class Notesapp extends StatelessWidget {
+  const Notesapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: 
+    );
+  }
+}
+
